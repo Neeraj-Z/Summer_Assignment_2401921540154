bc93d278b41a2539623bdcde9f4f8f428796f663
Neeraj-Z <neeraj79abc@gmail.com>
2026-06-10 22:45:28 +0530

    DSA 2.3.1

---
+++ Week_2/Day_3/28_Find the Index of the First Occurrence in a String.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) {
+            return 0;
+        }
+        if (needle.length() > haystack.length()) {
+            return -1;
+        }
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            boolean found = true;
+            for (int j = 0; j < needle.length(); j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    found = false;
+                    break;
+                }
+            }
+            if (found) {
+                return i; 
+            }
+        }
+        
+        return -1;
+    }
+}
