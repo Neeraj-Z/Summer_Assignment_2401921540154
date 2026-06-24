f59c01993508e0e51feb20cf59c593453a5e9820
Neeraj-Z <neeraj79abc@gmail.com>
2026-06-24 10:15:49 +0530

    DSA 4.3.2

---
+++ Week_4/Day_3/98.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    List<Integer> list = new ArrayList<>();
+
+    public boolean isValidBST(TreeNode root) {
+
+        inorder(root);
+
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) <= list.get(i - 1)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        list.add(root.val);
+        inorder(root.right);
+    }
+}
